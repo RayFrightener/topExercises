25056736b01db2ceeabeec6841f360a7ff1922a2
Ray <pranshuchawla19@gmail.com>
2024-08-03 12:10:45 -0400

    bu

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,11 +1,21 @@
 import _ from 'lodash';
-import myName from './myName';
+import './style.css';
+import icon from './icon.png';
 
-function component() {
-    const element = document.createElement('div');
-    element.textContent = myName('Cody');
-    
-    return element;
-}
+ function component() {
+   const element = document.createElement('div');
 
-document.body.appendChild(component());
+   // Lodash, now imported by this script
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  element.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = icon;
+
+  element.appendChild(myIcon);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
